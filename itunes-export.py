4d147a01f288995f787cb55f2f671d326aa2e9ac
Peter Stubbs <peter.stubbs@outlook.com>
2025-02-14 11:04:05 +0000

    so confused

--- itunes-export.py
+++ itunes-export.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from libpytunes import Library
 from libpytunes import Playlist
 from pathlib import Path
@@ -8,6 +9,7 @@ parser = argparse.ArgumentParser(description="An utility application to export i
 parser.add_argument("--output", "-o", help="The outpout folder for exporting the playlists.", required=True)
 parser.add_argument("--ignore", help="Ignore a specific playlist.", action='append')
 parser.add_argument("--library", "-l", help="The path to the iTunes Library XML.", default=str(Path.home().joinpath("Music/iTunes/iTunes Music Library.xml")))
+parser.add_argument("--relative", "-r", action='store_true', help="Use paths relative to the iTunes library in the playlists")
 parser.add_argument("--export-genius-playlists", action='store_true', dest='exportGeniusPlaylists')
 parser.add_argument("--export-smart-playlists", action='store_true', dest='exportSmartPlaylists')
 args = parser.parse_args()
@@ -20,33 +22,36 @@ def cleanupPlaylistName(playlistName):
         return playlistName.replace("/", "").replace("\\", "").replace(":", "")
 
 def exportPlaylist(playlist: Playlist, parentPath: Path):
-        if(playlist.is_genius_playlist and not args.exportGeniusPlaylists):
-                return
+    if(playlist.is_genius_playlist and not args.exportGeniusPlaylists):
+        return
 
-        if(playlist.is_smart_playlist and not args.exportSmartPlaylists):
-                return
+    if(playlist.is_smart_playlist and not args.exportSmartPlaylists):
+        return
 
-        if(playlist.is_folder):
-                # Create Folder
-                currentPath = parentPath.joinpath(playlist.name)
-                if(not currentPath.exists()):
-                        currentPath.mkdir()
+    if(playlist.is_folder):
+        # Create Folder
+        currentPath = parentPath.joinpath(playlist.name)
+        if(not currentPath.exists()):
+            currentPath.mkdir()
 
-                for childPlaylist in playlists.values():
-                        if(childPlaylist.parent_persistent_id == playlist.playlist_persistent_id):
-                                exportPlaylist(childPlaylist, currentPath)
-        else:
-                playlistContent = ""
-                for track in playlist.tracks:
-                        if track.location != None:
-                                try:
-                                        playlistContent +=  os.path.relpath(track.location, start=parentPath) + "\n"
-                                except ValueError:
-                                        print("Warning: Could not add the track \"" + track.location + "\" as relative path to the playlist \"" + playlistName + "\"; added the track as absolute path instead.")
-                                        playlistContent += track.location + "\n"
+        for childPlaylist in list(playlists.values()):
+            if(childPlaylist.parent_persistent_id == playlist.playlist_persistent_id):
+                exportPlaylist(childPlaylist, currentPath)
+    else:
+        playlistContent = ""
+        for track in playlist.tracks:
+            if args.relative:
+                if track.location != None:
+                    try:
+                        playlistContent +=  os.path.relpath(track.location, start=parentPath) + "\n"
+                    except ValueError:
+                        print(("Warning: Could not add the track \"" + track.location + "\" as relative path to the playlist \"" + playlistName + "\"; added the track as absolute path instead."))
+                        playlistContent += track.location + "\n"
+            else:
+                playlistContent += "/" + track.location + "\n"
 
-                playlistPath = parentPath.joinpath(cleanupPlaylistName(playlist.name) + ".m3u")
-                playlistPath.write_text(playlistContent, encoding="utf8")
+            playlistPath = parentPath.joinpath(cleanupPlaylistName(playlist.name) + ".m3u")
+            playlistPath.write_text(playlistContent, encoding="utf8")
 
 playlists = {}
 
@@ -57,6 +62,6 @@ for playlistName in library.getPlaylistNames(ignoreList=[
     playlist = library.getPlaylist(playlistName)
     playlists[playlist.playlist_persistent_id] = playlist
 
-for playlist in playlists.values(): 
+for playlist in list(playlists.values()): 
     if(playlist.parent_persistent_id == None) :
         exportPlaylist(playlist, playlistRootPath)
--- libpytunes/__init__.py
+++
@@ -1,3 +0,0 @@
-from libpytunes.Library import Library
-from libpytunes.Song import Song
-from libpytunes.Playlist import Playlist
